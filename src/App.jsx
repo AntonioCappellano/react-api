5d624f7fc409869ccc46875cd99c7d05b434cd21
AntonioCappellano <antoniocappellano97@gmail.com>
2026-02-15 17:14:18 +0100

    fix: correct bootstrap import path and add axios dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 export default function App() {
+  const [actors, setActors] = useState([]);
+
+  function fetchActor() {
+    axios.get("https://lanciweb.github.io/demo/api/actors/").then((res) => {
+      const actorList = res.data;
+      console.log(actorList);
+      setActors(actorList);
+    });
+  }
+
+  useEffect(fetchActor, []);
+
   return <></>;
 }
